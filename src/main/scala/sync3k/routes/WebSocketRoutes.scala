c54d16c1bc9853888d7d11cdb848b369da9f7eed
In-Ho Yi <chajath@gmail.com>
2017-07-07 00:58:38 -0700

    Use MergeHub to simulate ordered pubsub endpoint

--- src/main/scala/sync3k/routes/WebSocketRoutes.scala
+++ src/main/scala/sync3k/routes/WebSocketRoutes.scala
@@ -1,49 +1,47 @@
 package sync3k.routes
 
-import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.PathMatchers.IntNumber
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.WebSocketDirectives
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub, Source}
 
-import scala.collection.mutable.ObservableBuffer
 import scala.collection.mutable.ArrayBuffer
 
 trait WebSocketRoutes extends WebSocketDirectives {
   implicit val materializer: Materializer
-  //val items: ObservableBuffer[String] = new ArrayBuffer[String] with ObservableBuffer[String] {}
-  val items = ArrayBuffer[String]()
+  private val items = ArrayBuffer[(String, Int)]()
+  lazy val (sink, source) =
+    MergeHub.source[String]
+      .zip(Source(Stream.from(1)))
+      .toMat(BroadcastHub.sink)(Keep.both)
+      .run()
 
-  def greeter(n: Int): Flow[Message, Message, Any] =
-    Flow[Message].mapConcat {
-      case tm: TextMessage =>
-        tm.textStream.runForeach((item) => {
-          items += item
-        })
-        items.drop(n).toList.map((item) => TextMessage(item))
-      case bm: BinaryMessage =>
-        bm.dataStream.runWith(Sink.ignore)
-        Nil
-    }
-
-  def wssup: Flow[Message, Message, Any] =
-    Flow.fromSinkAndSource[Message, Message](Sink.foreach({
-      case tm: TextMessage =>
-        tm.textStream.runForeach((item) => {
-          items += item
-        })
-    }), Source
-        .fromIterator(() => items.iterator.map((item) => TextMessage(item)))
-        //.concat(Source.fromPublisher(items).map((item) => TextMessage(item)))
+  def wssup(skip: Int = 0): Flow[Message, Message, Any] =
+    Flow.fromSinkAndSource[Message, Message](
+      Flow[Message].flatMapConcat({
+        case tm: TextMessage =>
+          tm.textStream
+      }).to(sink),
+      Source(items.toList)
+        .concat(source)
+        .filterNot(_._2 <= skip)
+        .map({ case (item, id) => TextMessage(s"$id: $item") })
     )
 
-  lazy val webSocketRoutes: Route =
-    path("ws" / IntNumber) {
-      (n) => handleWebSocketMessages(greeter(n))
-    } ~ path("wssup") {
-      handleWebSocketMessages(wssup)
+  lazy val webSocketRoutes: Route = {
+    source.runForeach((item) => {
+      items += item
+    })
+    pathPrefix("ws") {
+      pathEndOrSingleSlash {
+        handleWebSocketMessages(wssup())
+      } ~ path(IntNumber) {
+        (n) => handleWebSocketMessages(wssup(n))
+      }
     }
+  }
 }

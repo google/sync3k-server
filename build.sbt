c68f896c455455373ef588a22477f6218b2cdf4b
In-Ho Yi <chajath@gmail.com>
2017-07-09 16:13:47 -0700

    Add kafka binding

--- build.sbt
+++ build.sbt
@@ -1,5 +1,6 @@
 lazy val akkaHttpVersion = "10.0.9"
 lazy val akkaVersion    = "2.5.3"
+lazy val akkaKafkaVersion = "0.16"
 
 lazy val root = (project in file(".")).
   settings(
@@ -13,6 +14,7 @@ lazy val root = (project in file(".")).
       "com.typesafe.akka" %% "akka-http-xml"     % akkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
       "com.typesafe.akka" %% "akka-stream"       % akkaVersion,
+      "com.typesafe.akka" %% "akka-stream-kafka" % akkaKafkaVersion,
       "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
       "org.scalatest"     %% "scalatest"         % "3.0.1"         % Test
     )
--- src/main/scala/sync3k/routes/WebSocketRoutes.scala
+++ src/main/scala/sync3k/routes/WebSocketRoutes.scala
@@ -1,18 +1,22 @@
 package sync3k.routes
 
+import akka.actor.ActorSystem
 import akka.http.scaladsl.model.ws.{ Message, TextMessage }
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.PathMatchers.IntNumber
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.WebSocketDirectives
+import akka.kafka.scaladsl.{ Consumer, Producer }
+import akka.kafka.{ ConsumerSettings, ProducerSettings, Subscriptions }
 import akka.stream.Materializer
 import akka.stream.scaladsl.{ BroadcastHub, Flow, Keep, MergeHub, Source }
-
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.{ ByteArrayDeserializer, ByteArraySerializer, StringDeserializer, StringSerializer }
 import spray.json._
 
 import scala.collection.mutable.ArrayBuffer
-import scala.concurrent.duration._
 
 trait WebSocketRoutes extends WebSocketDirectives {
   implicit val materializer: Materializer
@@ -26,7 +30,7 @@ trait WebSocketRoutes extends WebSocketDirectives {
       .toMat(BroadcastHub.sink)(Keep.both)
       .run()
 
-  case class OrderedMessage(id: Int, message: String)
+  case class OrderedMessage(id: Long, message: String)
   object OrderedMessageProtocol extends DefaultJsonProtocol {
     implicit val orderedMessageFormat = jsonFormat2(OrderedMessage.apply)
   }
@@ -41,9 +45,37 @@ trait WebSocketRoutes extends WebSocketDirectives {
       }).to(sink),
       Source(items.toList)
         .concat(source)
-        .filterNot(_._2 <= skip)
+        .filterNot(_._2 < skip)
         .map({ case (item, id) => TextMessage(OrderedMessage(id, item).toJson.toString) })
     // .keepAlive(30.seconds, () => TextMessage("\"no-op\""))
+    )
+
+  implicit val system: ActorSystem
+
+  private lazy val producerSettings = ProducerSettings(
+    system,
+    new ByteArraySerializer,
+    new StringSerializer
+  ).withBootstrapServers("localhost:9092")
+
+  private lazy val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+    .withBootstrapServers("localhost:9092")
+    .withGroupId("group1")
+
+  def wsKafka(topic: String, offset: Long = 0): Flow[Message, Message, Any] =
+    Flow.fromSinkAndSource[Message, Message](
+      Flow[Message].flatMapConcat({
+        case tm: TextMessage =>
+          tm.textStream
+      })
+        .map((item) => new ProducerRecord[Array[Byte], String](topic, item))
+        .to(Producer.plainSink(producerSettings)),
+      Consumer.plainSource(consumerSettings, Subscriptions.assignmentWithOffset(
+        new TopicPartition(topic, 0) -> offset
+      ))
+        .map((message) => {
+          TextMessage(OrderedMessage(message.offset(), message.value()).toJson.toString())
+        })
     )
 
   lazy val webSocketRoutes: Route = {
@@ -53,6 +85,8 @@ trait WebSocketRoutes extends WebSocketDirectives {
       } ~ path(IntNumber) {
         (n) => handleWebSocketMessages(wssup(n))
       }
+    } ~ path("kafka" / Segment / LongNumber) {
+      (topic, n) => handleWebSocketMessages(wsKafka(topic, n))
     }
   }
 }
